1d6b094b4fa49ca6f2e98477fc4e868cd9ae47af
doublejarvis <doublejarvis@gmail.com>
2019-06-06 21:18:48 +0300

    store player object in state

--- src/Stream.js
+++ src/Stream.js
@@ -1,26 +1,44 @@
 import React, { Component } from 'react';
 
 class Stream extends Component {
+  constructor(props) {
+    super(props);
+    this.onMouseEnter = this.onMouseEnter.bind(this)
+    this.onMouseLeave = this.onMouseLeave.bind(this)
+    this.state = {
+      player: null
+    }
+  }
   componentDidMount() {
     var options = {
       width: "100%",
       height: 400,
       channel: this.props.channel,
-    };
-    var player = new window.Twitch.Player(this.props.channel, options);
-    player.setVolume(0.5);
+    }
+    var player = new window.Twitch.Player(this.props.channel, options)
+    player.setVolume(this.props.settings.volume)
+    player.setQuality(this.props.settings.lowQuality)
+
+    this.setState({ player: player })
   }
   render() {
-    const { channel, onclick } = this.props;
+    const { channel } = this.props;
     return (
-      <div style={streamStyles} id={channel}>
-        <button onclick={ onclick }>Remove</button>
+      <div onMouseEnter={ this.onMouseEnter } onMouseLeave={ this.onMouseLeave } className="stream" id={ channel }>
+        <button id={ channel }>Remove</button>
       </div>
     )
   }
-}
-const streamStyles = {
-  border: "3px dashed #333"
+  onMouseEnter(event) {
+    this.state.player.setQuality(this.props.settings.highQuality)
+    this.state.player.setMuted(false)
+  }
+  onMouseLeave(event) {
+    if (!this.state.player.getFullscreen()) {
+      this.state.player.setQuality(this.props.settings.lowQuality)
+      this.state.player.setMuted(true)
+    }
+  }
 }
 
 export default Stream;

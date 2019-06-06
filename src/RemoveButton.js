a83a6a3a5bd9c7b21e9c481d2016b6f6085d00b6
doublejarvis <doublejarvis@gmail.com>
2019-06-06 21:53:34 +0300

    add RemoveButton component

---
+++ src/RemoveButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class RemoveButton extends Component {
+  render() {
+    const { id } = this.props
+    return (
+      <button className="btn" id={id} onClick={() => this.onRemoveClick(id)}>
+        Remove
+      </button>
+    )
+  }
+  onRemoveClick(channel) {
+    this.props.onRemoveClick(channel)
+  }
+}
+
+export default RemoveButton;

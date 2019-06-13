6629c118250abe9cedca53818d4f57ed6a35ed58
doublejarvis <doublejarvis@gmail.com>
2019-06-13 10:29:31 +0300

    Updates

---
+++ precache-manifest.dbc0eaf386534e056a0017aa347da118.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "840388fb4ec6b03b2973f771f6a9ff6c",
+    "url": "/multijarvis-react/index.html"
+  },
+  {
+    "revision": "8733aea77c4f6d3d723b",
+    "url": "/multijarvis-react/static/css/main.bfe06db8.chunk.css"
+  },
+  {
+    "revision": "ded6086e3edcf86cf3ce",
+    "url": "/multijarvis-react/static/js/2.f3b64ad3.chunk.js"
+  },
+  {
+    "revision": "8733aea77c4f6d3d723b",
+    "url": "/multijarvis-react/static/js/main.cd7192ed.chunk.js"
+  },
+  {
+    "revision": "1f3c6d7bd7b1f4148019",
+    "url": "/multijarvis-react/static/js/runtime~main.ce6311d0.js"
+  }
+]);
